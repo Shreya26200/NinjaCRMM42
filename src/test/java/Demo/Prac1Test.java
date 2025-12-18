81180978bc861b4244e31929575883216fa2c9af
Admin <Admin@Shreya-Elitebook>
2025-12-18 07:44:32 +0530

    First

---
+++ src/test/java/Demo/Prac1Test.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Prac1Test {
+	
+	@Test (groups="Smoke")
+	public void CarTest()
+	{
+		System.out.println("Audi");
+	}
+
+}
---
+++ src/test/java/Demo/PracTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class PracTest {
+	
+	@Test (groups="Regression")
+	public void BikeTest()
+	{
+		System.out.println("Yamaha");
+	}
+
+}
